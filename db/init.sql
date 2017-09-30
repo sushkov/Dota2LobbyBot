d95b353d71a3ccb3e1baaf6d86fbbe5e2d95fb84
Vladimir Sushkov <volodya.sushkov@gmail.com>
2017-09-30 03:13:40 +0300

    add comments to database structure

--- db/init.sql
+++ db/init.sql
@@ -1,11 +1,13 @@
 -- Database structure
 
+-- lobby settings table
 create table settings(
     id int not null auto_increment,
     leagueid int,
     primary key (id)
 );
 
+-- lobby table
 create table lobby (
     id bigint not null auto_increment,
     name text,
@@ -15,13 +17,16 @@ create table lobby (
     primary key(id),
     foreign key (settings_id) references settings(id)
 );
+-- lobby.status: 1 - match started; 2 - lobby in process; 3 - bot leave lobby by timeout
 
+-- users
 create table user (
     id bigint not null auto_increment,
     `steam-id` text,
     primary key(id)
 );
 
+-- users in lobbies
 create table lobby_user (
     id bigint not null auto_increment,
     lobby_id bigint,
@@ -31,6 +36,7 @@ create table lobby_user (
     foreign key (user_id) references user(id)
 );
 
+-- bot accounts
 create table bot(
     id int not null auto_increment,
     steam_name text,
